6a75e80d83b29354d4621e01c8dbfbfee8b058ba
Neilson K-S <neilson.ks@gmail.com>
2023-06-12 17:22:33 -0400

    buttons

--- packages/client/Assets/Scripts/Game/Structure.cs
+++ packages/client/Assets/Scripts/Game/Structure.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Structure : Entity
 {
     public override void OnDrawGizmosSelected() {
-        #if UNITY_EDITOR
-        UnityEditor.Handles.Label(transform.position + Vector3.up * 1f, "World Pos: " + transform.position.ToString() + "\nGrid Pos: " + gridPos.ToString() + "\nBlock Pos: " + MapGenerator.Instance.WorldToGrid(transform.position));
-        #endif
+        // #if UNITY_EDITOR
+        // UnityEditor.Handles.Label(transform.position + Vector3.up * 1f, "World Pos: " + transform.position.ToString() + "\nGrid Pos: " + gridPos.ToString() + "\nBlock Pos: " + MapGenerator.Instance.WorldToGrid(transform.position));
+        // #endif
     }
 }
