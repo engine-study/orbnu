b17f4552703b3f89db31e59b4d40f567cf7d11cc
Neilson K-S <neilson.ks@gmail.com>
2023-06-22 15:31:12 -0700

    building component

---
+++ packages/client/Assets/Scripts/MUDGame/MUDComponentBuilding.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Building", menuName = "MUD/Components/Building", order = 1)]
+public class MUDComponentBuilding : MUDComponent
+{
+    public override void InitFromTable()
+    {
+        throw new System.NotImplementedException();
+    }
+}
