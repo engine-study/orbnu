f5192f6ce02196bce4fa4a67f0fee973340db5f4
Neilson K-S <neilson.ks@gmail.com>
2023-06-22 17:46:39 -0700

    build pos

--- packages/contracts/src/positionToEntityKey.sol
+++ packages/contracts/src/positionToEntityKey.sol
@@ -5,6 +5,9 @@ function positionToEntityKey(uint32 x, uint32 y) pure returns (bytes32) {
   return keccak256(abi.encode(x, y));
 }
 
+function positionToEntityKey(int32 x, int32 y) pure returns (bytes32) {
+  return keccak256(abi.encode(x, y));
+}
 
 function position3DToEntityKey(uint32 x, uint32 y, uint32 z) pure returns (bytes32) {
   return keccak256(abi.encode(x, y, z));
